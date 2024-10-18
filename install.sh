27afc15d1909ffa5de4915ea48a98a57dafaf824
0bfu5c4t3 <muhidiny@icloud.com>
2024-10-18 09:38:07 +0300

    Initial commit

---
+++ install.sh
@@ -0,0 +1,102 @@
+#!/bin/bash
+
+# Check if running as root. If root, script will exit
+if [[ $EUID -eq 0 ]]; then
+    echo "This script should not be executed as root! Exiting......."
+    exit 1
+fi
+
+clear
+
+# Set some colors for output messages
+OK="$(tput setaf 2)[OK]$(tput sgr0)"
+ERROR="$(tput setaf 1)[ERROR]$(tput sgr0)"
+NOTE="$(tput setaf 3)[NOTE]$(tput sgr0)"
+WARN="$(tput setaf 166)[WARN]$(tput sgr0)"
+CAT="$(tput setaf 6)[ACTION]$(tput sgr0)"
+ORANGE=$(tput setaf 166)
+YELLOW=$(tput setaf 3)
+RESET=$(tput sgr0)
+
+# Display welcome message
+echo "Diny's Arch Hyprland Dotfiles Installer"
+echo
+
+# Set default values for user responses
+aur_helper="yay"
+nvidia="N"
+gtk_themes="Y"
+bluetooth="Y"
+thunar="Y"
+rog="N"
+sddm="N"
+xdph="Y"
+zsh="N"
+dots="Y"
+
+# Set the name of the log file to include the current date and time
+LOG="install-$(date +%d-%H%M%S).log"
+
+# Define the directory where your scripts are located
+script_directory=install-scripts
+
+# Function to execute a script if it exists and make it executable
+execute_script() {
+    local script="$1"
+    local script_path="$script_directory/$script"
+    if [ -f "$script_path" ]; then
+        chmod +x "$script_path"
+        if [ -x "$script_path" ]; then
+            "$script_path"
+        else
+            echo "Failed to make script '$script' executable."
+        fi
+    else
+        echo "Script '$script' not found in '$script_directory'."
+    fi
+}
+
+# Ensuring all scripts in the scripts folder are executable
+chmod +x install-scripts/*
+
+# Execute AUR helper script based on user choice
+if [ "$aur_helper" == "paru" ]; then
+    execute_script "paru.sh"
+elif [ "$aur_helper" == "yay" ]; then
+    execute_script "yay.sh"
+fi
+
+# Install hyprland packages
+execute_script "00-hypr-pkgs.sh"
+
+if [ "$gtk_themes" == "Y" ]; then
+    execute_script "gtk_themes.sh"
+fi
+
+if [ "$bluetooth" == "Y" ]; then
+    execute_script "bluetooth.sh"
+fi
+
+if [ "$thunar" == "Y" ]; then
+    execute_script "thunar.sh"
+fi
+
+if [ "$rog" == "Y" ]; then
+    execute_script "rog.sh"
+fi
+
+if [ "$xdph" == "Y" ]; then
+    execute_script "xdph.sh"
+fi
+
+if [ "$zsh" == "Y" ]; then
+    execute_script "zsh.sh"
+fi
+
+if [ "$dots" == "Y" ]; then
+    execute_script "dotfiles.sh" "$(pwd)"
+fi
+
+clear
+
+printf "\n${OK} Installation Completed Successfully.\n"
